298c1ed062c0ae97945866973c6d6cf756d796f3
Florin Munteanu <florinmunteanu96@gmail.com>
2017-11-27 11:13:12 +0200

    Reset error when credentials are changed
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- src/main/java/DeliveryCompany/LogInController.java
+++ src/main/java/DeliveryCompany/LogInController.java
@@ -1,19 +1,42 @@
 package DeliveryCompany;
 
-import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
 
 public class LogInController {
     @FXML
     private Label errorMessageLabel;
+    @FXML
+    private JFXTextField userNameField;
+    @FXML
+    private JFXPasswordField passwordField;
+
     public LogInController() {}
 
     @FXML
-    private void initialize() {}
-
+    private void initialize() {
+        addListenerWhenCredentialsAreChanged();
+    }
+    
     @FXML
     private void logIn(){
         errorMessageLabel.setVisible(true);
+    }
+
+    private void addListenerWhenCredentialsAreChanged() {
+        userNameField.textProperty().addListener(e->{
+            resetError();
+        });
+        passwordField.textProperty().addListener(e->{
+            resetError();
+        });
+    }
+    private void resetError(){
+        errorMessageLabel.setVisible(false);
     }
 }
